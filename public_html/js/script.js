c4ef52c224375170bb4e4cca3bf4564d6e5542bf
Carlos <Carlos@xavier>
2018-01-02 22:55:10 -0200

    Efeito de aparecer e desaparecer nos botões laterais

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -6,16 +6,35 @@ $(document).ready(function () {
     /*Carrega a página inicio.html quando o site é aberto*/
     $("#conteudo").load("inicio.html");
     /*Chama a página acessorios.html quando o botaoAcessorios for clicado*/
-    $("#botaoAcessorios").click(function () {
-        $("#conteudo").load("Acessorios.html");
+    $(".botaoAcessorios").click(function () {
+        $("#conteudo").load("acessorios.html");
+        $("#botaoLateralAcessorios").css("color","#ffb5ba");//Ainda o botão não fica rosa quando clica nele - botão lateral de acessórios
     });
     /*Chama a página inicio.html quando o botao inicio for clicado*/
     $("#inicio").click(function () {
         $("#conteudo").load("inicio.html");
     });
-    $(".botaoRoupaFeminina").click(function(){
-       $("#conteudo").load("RoupasMeninas.html");
+    /*Chama a página roupasMeninas via ajax*/
+    $(".botaoRoupaFeminina").click(function () {
+        $("#conteudo").load("roupasMeninas.html");
     });
+    /*Faz aparecer e desaparecer os botões laterais das meninas*/
+    $("#meninas").click(function () {
+        $(this).css("color", "black");
+        $("#botaoLateralSapatosFemininos").slideToggle("slow", function () {
+            $("#botaoLateralRoupasFemininas").slideToggle("slow");
+        });
+    });
+    /*Faz aparecer e desaparecer os botões laterais dos meninos*/
+    $("#meninos").click(function () {
+        $(this).css("color", "black");
+        $("#botaoLateralSapatosMasculinos").slideToggle("slow", function () {
+            $("#botaoLateralRoupasMasculinas").slideToggle("slow");
+        });
+    });
+
+
+
     /*TROCAR AS IMAGENS DE FRETE E COMPRA POR UM BOTÃO DE COMPRAS COM UM CARRINHO*/
 
 
