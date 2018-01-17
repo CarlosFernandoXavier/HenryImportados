fee39d1bf56f5ee304bc67714f632d07ed70662e
Carlos <Carlos@xavier>
2018-01-17 19:39:23 -0200

    Colocação da função que dispara um clique no botaoMenuMobile, fazendo com que ele feche quando o usuário escolhe alguma opçao do menu

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -9,48 +9,61 @@ $(document).ready(function () {
     $(".botaoAcessorios").click(function () {
         $("#conteudo").load("acessorios.html");
         $("#botaoLateralAcessorios").css("color", "#ffb5ba");//Ainda o botão não fica rosa quando clica nele - botão lateral de acessórios
+        $("#botaoMenuMobile").trigger("click");//dispara um clique no botao do menu, fechando o mesmo
     });
     /*Chama a página inicio.html quando o botao inicio for clicado*/
     $(".inicio").click(function () {
         $("#conteudo").load("inicio.html");
+        $("#botaoMenuMobile").trigger("click");//dispara um clique no botao do menu, fechando o mesmo
     });
+    /*$("#botaoMenuMobile").click(function(){
+     $("#myNavbar").slideDown();
+     });*/
     /*Chama a página roupasMeninas via ajax*/
     $(".botaoRoupaFeminina").click(function () {
         $("#conteudo").load("roupasMeninas.html");
+        $("#botaoMenuMobile").trigger("click");//dispara um clique no botao do menu, fechando o mesmo
     });
     /*Chama a página sapatosFemininos via ajax*/
     $(".botaoSapatosFemininos").click(function () {
         $("#conteudo").load("sapatosFemininos.html");
+        $("#botaoMenuMobile").trigger("click");//dispara um clique no botao do menu, fechando o mesmo
     });
 
     /*Faz aparecer e desaparecer os botões laterais das meninas*/
     $("#meninas").click(function () {
         $(this).css("color", "black");
         $("#botaoLateralSapatosFemininos").slideToggle("slow", function () {
-            $("#botaoLateralRoupasFemininas").slideToggle("slow");
+            $("#botaoLateralRoupasFemininas").slideToggle("slow", function () {
+                $("#botaoMenuMobile").trigger("click");//dispara um clique no botao do menu, fechando o mesmo
+            });
         });
     });
     /*Faz aparecer e desaparecer os botões laterais dos meninos*/
     $("#meninos").click(function () {
         $(this).css("color", "black");
         $("#botaoLateralSapatosMasculinos").slideToggle("slow", function () {
-            $("#botaoLateralRoupasMasculinas").slideToggle("slow");
+            $("#botaoLateralRoupasMasculinas").slideToggle("slow", function () {
+                $("#botaoMenuMobile").trigger("click");//dispara um clique no botao do menu, fechando o mesmo
+            });
         });
     });
     //Tira o sublinhado de elementos que estejam dento da tag <a>, assim o
     //o texto "Menu" do menu mobile fica sem o underline
-    $("a").css("textDecoration","none");
-    
+    $("a").css("textDecoration", "none");
+
     /*Chama a página roupasMeninos via ajax*/
     $(".botaoRoupasMasculinas").click(function () {
         $("#conteudo").load("roupasMeninos.html");
+        $("#botaoMenuMobile").trigger("click");//dispara um clique no botao do menu, fechando o mesmo
     });
     /*Chama a página sapatosMasculinos via ajax*/
     $(".botaoSapatosMasculinos").click(function () {
         $("#conteudo").load("sapatoMasculino.html");
+        $("#botaoMenuMobile").trigger("click");//dispara um clique no botao do menu, fechando o mesmo
     });
     /*Cor chocolate nas subopções do 1º menu - versão mobile*/
-    $("#myNavbar ul li ul li a").css("color","#d2691e");
+    $("#myNavbar ul li ul li a").css("color", "#d2691e");
     /*Efeito de fundo rosa claro quando uma opção do menu mobile é clicada*/
     $("#1").click(function () {
         $(this).css("backgroundColor", "#ffe4c4");
@@ -91,6 +104,7 @@ $(document).ready(function () {
         $("#3").css("backgroundColor", "#faebd7");
         $("#4").css("backgroundColor", "#faebd7");
         $("#6").css("backgroundColor", "#faebd7");
+        $("#botaoMenuMobile").trigger("click");//dispara um clique no botao do menu, fechando o mesmo
     });
     $("#6").click(function () {
         $(this).css("backgroundColor", "#ffe4c4");
@@ -99,21 +113,22 @@ $(document).ready(function () {
         $("#3").css("backgroundColor", "#faebd7");
         $("#4").css("backgroundColor", "#faebd7");
         $("#5").css("backgroundColor", "#faebd7");
+        $("#botaoMenuMobile").trigger("click");//dispara um clique no botao do menu, fechando o mesmo
     });
     /*Efeito sombreado quando o botão menu mobile para indicar quando ele está clicado
      * ou não*/
-    var cliqueBotao=false;
-    $("button#botaoMenuMobile").click(function(){
-        if(!cliqueBotao){
-            $(this).css("boxShadow","0px 0px 4px black");
-            cliqueBotao=true;
-        }else{
-            $(this).css("boxShadow","none");
-            cliqueBotao=false;
+    var cliqueBotao = false;
+    $("button#botaoMenuMobile").click(function () {
+        if (!cliqueBotao) {
+            $(this).css("boxShadow", "0px 0px 4px black");
+            cliqueBotao = true;
+        } else {
+            $(this).css("boxShadow", "none");
+            cliqueBotao = false;
         }
     });
 
-   
+
 
     /*Faz o efeito de zoom nas imagens
      $(".thumbnail a img").hover(function () {
